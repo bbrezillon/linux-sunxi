fe70556e1243e865b74b0ba24190755f58658336
Ruslan Bilovol <ruslan.bilovol@gmail.com>
2015-03-12 23:43:46 +0200

    usb: gadget: udc-core: independent registration of gadgets and gadget drivers
    Change behavior during registration of gadgets and
    gadget drivers in udc-core. Instead of previous
    approach when for successful probe of usb gadget driver
    at least one usb gadget should be already registered
    use another one where gadget drivers and gadgets
    can be registered in udc-core independently.
    
    Independent registration of gadgets and gadget drivers
    is useful for built-in into kernel gadget and gadget
    driver case - because it's possible that gadget is
    really probed only on late_init stage (due to deferred
    probe) whereas gadget driver's probe is silently failed
    on module_init stage due to no any UDC added.
    
    Also it is useful for modules case - now there is no
    difference what module to insert first: gadget module
    or gadget driver one.
    
    Signed-off-by: Ruslan Bilovol <ruslan.bilovol@gmail.com>
    Signed-off-by: Maxime Ripard <maxime.ripard@free-electrons.com>

--- include/linux/usb/gadget.h
+++ include/linux/usb/gadget.h
@@ -991,6 +991,7 @@ static inline int usb_gadget_activate(struct usb_gadget *gadget)
  * @driver: Driver model state for this driver.
  * @udc_name: A name of UDC this driver should be bound to. If udc_name is NULL,
  * 	this driver will be bound to any available UDC.
+ * @pending: UDC core private data used for deferred probe of this driver.
  *
  * Devices are disabled till a gadget driver successfully bind()s, which
  * means the driver will handle setup() requests needed to enumerate (and
@@ -1053,6 +1054,7 @@ struct usb_gadget_driver {
 	struct device_driver	driver;
 
 	char			*udc_name;
+	struct list_head	pending;
 };
 
 
