a2f1fc69f0806e009a2fee9a668df1256effea95
Boris Brezillon <boris.brezillon@free-electrons.com>
2015-06-23 12:01:52 +0200

    mtd: nand: hynix: add read-retry support for H27Q chips
    Signed-off-by: Boris Brezillon <boris.brezillon@free-electrons.com>

--- drivers/mtd/nand/nand_hynix.c
+++ drivers/mtd/nand/nand_hynix.c
@@ -20,9 +20,14 @@ static u8 h27ucg8t2a_read_retry_regs[] = {
 	0xcc, 0xbf, 0xaa, 0xab, 0xcd, 0xad, 0xae, 0xaf
 };
 
+static u8 h27q_read_retry_regs[] = {
+	0x38, 0x39, 0x3a, 0x3b,
+};
+
 struct hynix_read_retry {
+	int nregs;
 	u8 *regs;
-	u8 values[64];
+	u8 values[0];
 };
 
 struct hynix_nand {
@@ -33,12 +38,12 @@ int nand_setup_read_retry_hynix(struct mtd_info *mtd, int retry_mode)
 {
 	struct nand_chip *chip = mtd->priv;
 	struct hynix_nand *hynix = chip->manuf_priv;
-	int offset = retry_mode * 8;
+	int offset = retry_mode * hynix->read_retry.nregs;
 	int status;
 	int i;
 
 	chip->cmdfunc(mtd, 0x36, -1, -1);
-	for (i = 0; i < 8; i++) {
+	for (i = 0; i < hynix->read_retry.nregs; i++) {
 		int column = hynix->read_retry.regs[i];
 		column |= column << 8;
 		chip->cmdfunc(mtd, NAND_CMD_NONE, column, -1);
@@ -53,7 +58,7 @@ int nand_setup_read_retry_hynix(struct mtd_info *mtd, int retry_mode)
 	return 0;
 }
 
-static void h27ucg8t2a_cleanup(struct mtd_info *mtd)
+static void h27_cleanup(struct mtd_info *mtd)
 {
 	struct nand_chip *chip = mtd->priv;
 	kfree(chip->manuf_priv);
@@ -106,22 +111,174 @@ static int h27ucg8t2a_init(struct mtd_info *mtd, const uint8_t *id)
 	chip->select_chip(mtd, -1);
 
 	if (!ret) {
-		hynix = kzalloc(sizeof(*hynix), GFP_KERNEL);
+		hynix = kzalloc(sizeof(*hynix) + (8 * 8),
+				GFP_KERNEL);
 		if (!hynix) {
 			ret = -ENOMEM;
 			goto leave;
 		}
 
+		hynix->read_retry.nregs = 8;
 		hynix->read_retry.regs = h27ucg8t2a_read_retry_regs;
 		memcpy(hynix->read_retry.values, buf, 64);
 		chip->manuf_priv = hynix;
 		chip->setup_read_retry = nand_setup_read_retry_hynix;
 		chip->read_retries = 8;
-		chip->manuf_cleanup = h27ucg8t2a_cleanup;
+		chip->manuf_cleanup = h27_cleanup;
 	}
 
 leave:
 	kfree(buf);
+
+	return ret;
+}
+
+static int h27q_get_best_val(const u8 *buf, int size, int min_cnt)
+{
+	int *val, *cnt, best = -1, max_cnt = 0;
+	int i, j;
+
+	val = kzalloc(size * 2 * sizeof(int), GFP_KERNEL);
+	cnt = val + size;
+
+	for (i = 0; i < size; i++) {
+		if (val[i] < 0)
+			continue;
+
+		val[i] = buf[i];
+		cnt[i] = 1;
+
+		for (j = i + 1; j < size; j++) {
+			if (buf[j] == val[i]) {
+				val[j] = -1;
+				cnt[i]++;
+			}
+		}
+
+		if (max_cnt < cnt[i]) {
+			max_cnt = cnt[i];
+			best = i;
+		}
+	}
+
+	if (best >= 0)
+		best = val[best];
+
+	kfree(val);
+
+	if (best < 0 || max_cnt < min_cnt)
+		return -EINVAL;
+
+	return best;
+}
+
+#define H27Q_RR_TABLE_SIZE		784
+#define H27Q_RR_TABLE_NSETS		8
+
+static int h27q_init(struct mtd_info *mtd, const uint8_t *id)
+{
+	struct nand_chip *chip = mtd->priv;
+	struct hynix_nand *hynix = NULL;
+	u8 * buf = NULL, tmp_buf[H27Q_RR_TABLE_NSETS];
+	u8 total_rr_count, rr_reg_count;
+	int i, j, k;
+	int ret;
+
+	buf = kzalloc(H27Q_RR_TABLE_SIZE, GFP_KERNEL);
+	if (!buf)
+		return -ENOMEM;
+
+	chip->select_chip(mtd, 0);
+	chip->cmdfunc(mtd, NAND_CMD_RESET, -1, -1);
+	chip->cmdfunc(mtd, 0x36, 0x38, -1);
+	chip->write_byte(mtd, 0x52);
+	chip->cmdfunc(mtd, 0x16, -1, -1);
+	chip->cmdfunc(mtd, 0x17, -1, -1);
+	chip->cmdfunc(mtd, 0x04, -1, -1);
+	chip->cmdfunc(mtd, 0x19, -1, -1);
+	chip->cmdfunc(mtd, NAND_CMD_READ0, 0x0, 0x21f);
+
+	chip->read_buf(mtd, buf, H27Q_RR_TABLE_SIZE);
+
+	chip->cmdfunc(mtd, NAND_CMD_RESET, -1, -1);
+	chip->cmdfunc(mtd, 0x36, 0x38, -1);
+	chip->write_byte(mtd, 0x0);
+	chip->cmdfunc(mtd, 0x16, -1, -1);
+	chip->cmdfunc(mtd, NAND_CMD_READ0, 0x0, -1);
+	chip->select_chip(mtd, -1);
+
+	/* TODO: support multi-die chips */
+
+	ret = h27q_get_best_val(buf, 8, 5);
+	if (ret < 0)
+		goto err;
+	total_rr_count = ret;
+
+	ret = h27q_get_best_val(buf + 8, 8, 5);
+	if (ret < 0)
+		goto err;
+	rr_reg_count = ret;
+
+	if (rr_reg_count != sizeof(h27q_read_retry_regs)) {
+		ret = -EINVAL;
+		goto err;
+	}
+
+	hynix = kzalloc(sizeof(*hynix) +
+			(total_rr_count * rr_reg_count),
+			GFP_KERNEL);
+	if (!hynix) {
+		ret = -ENOMEM;
+		goto err;
+	}
+
+	for (i = 0; i < total_rr_count; i++) {
+		for (j = 0; j < rr_reg_count; j++) {
+			int offset = 16 + (i * rr_reg_count) + j;
+
+			for (k = 0; k < H27Q_RR_TABLE_NSETS; k++) {
+				int set_offset = k * rr_reg_count *
+						 total_rr_count * 2;
+
+				tmp_buf[k] = buf[offset + set_offset];
+			}
+
+			ret = h27q_get_best_val(tmp_buf, H27Q_RR_TABLE_NSETS,
+						5);
+			if (ret >= 0) {
+				hynix->read_retry.values[(i * rr_reg_count) + j] = ret;
+				continue;
+			}
+
+			offset += rr_reg_count * total_rr_count;
+			for (k = 0; k < H27Q_RR_TABLE_NSETS; k++) {
+				int set_offset = k * rr_reg_count *
+						 total_rr_count;
+
+				tmp_buf[k] = buf[offset + set_offset];
+			}
+			ret = h27q_get_best_val(tmp_buf, H27Q_RR_TABLE_NSETS,
+						5);
+			if (ret < 0)
+				goto err;
+			hynix->read_retry.values[(i * rr_reg_count) + j] = ~ret;
+		}
+	}
+
+	kfree(buf);
+
+	hynix->read_retry.nregs = rr_reg_count;
+	hynix->read_retry.regs = h27q_read_retry_regs;
+	chip->manuf_priv = hynix;
+	chip->setup_read_retry = nand_setup_read_retry_hynix;
+	chip->read_retries = total_rr_count;
+	chip->manuf_cleanup = h27_cleanup;
+
+	return 0;
+
+err:
+	kfree(buf);
+	kfree(hynix);
 
 	return ret;
 }
@@ -135,6 +292,10 @@ struct hynix_nand_initializer initializers[] = {
 	{
 		.id = {NAND_MFR_HYNIX, 0xde, 0x94, 0xda, 0x74, 0xc4},
 		.init = h27ucg8t2a_init,
+	},
+	{
+		.id = {NAND_MFR_HYNIX, 0xde, 0x14, 0xa7, 0x42, 0x4a},
+		.init = h27q_init,
 	},
 };
 
